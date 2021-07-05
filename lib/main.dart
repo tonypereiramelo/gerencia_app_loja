05f5b8ffc13fba605ec6a58f972f7fc662e53f61
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-07-05 08:02:13 -0300

    title

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gerenciamento Loja',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.pinkAccent,
