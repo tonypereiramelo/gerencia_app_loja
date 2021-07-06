06a93c46b9c1cd619cab76789d445fc28e3bfc66
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-07-06 13:05:10 -0300

    Validators funcionando

--- lib/blocs/login_bloc.dart
+++ lib/blocs/login_bloc.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:gerencia_app_loja/validators/login_validator.dart';
 import 'package:rxdart/rxdart.dart';
 
@@ -24,11 +26,19 @@ class LoginBloc extends BlocBase with LoginValidator {
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
 
+  // ignore: cancel_subscriptions
+  StreamSubscription? _streamSubscription;
+
   LoginBloc() {
-    FirebaseAuth.instance.authStateChanges().listen((user) {
+    _streamSubscription =
+        FirebaseAuth.instance.authStateChanges().listen((user) async {
       if (user != null) {
-        print('logou');
-        FirebaseAuth.instance.signOut();
+        if (await verifyPrivileges(user)) {
+          _stateController.add(LoginState.SUCESS);
+        } else {
+          FirebaseAuth.instance.signOut();
+          _stateController.add(LoginState.FAIL);
+        }
       } else {
         _stateController.add(LoginState.IDLE);
       }
@@ -51,11 +61,28 @@ class LoginBloc extends BlocBase with LoginValidator {
     });
   }
 
+  Future<bool> verifyPrivileges(User user) async {
+    return await FirebaseFirestore.instance
+        .collection('admins')
+        .doc(user.uid)
+        .get()
+        .then((doc) {
+      if (doc.data() != null) {
+        return true;
+      } else {
+        return false;
+      }
+    }).catchError((e) {
+      return false;
+    });
+  }
+
   @override
   void dispose() {
     super.dispose();
     _emailController.close();
     _passwordController.close();
     _stateController.close();
+    _streamSubscription!.cancel();
   }
 }
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -15,66 +15,84 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[850],
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(),
-          SingleChildScrollView(
-            child: Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.store_mall_directory,
-                    color: Colors.pinkAccent,
-                    size: 160,
+      body: StreamBuilder<LoginState>(
+          stream: _loginBloc.outState,
+          initialData: LoginState.LOADING,
+          builder: (context, snapshot) {
+            print(snapshot.data);
+            switch (snapshot.data!) {
+              case LoginState.LOADING:
+                return Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(Colors.pinkAccent),
                   ),
-                  InputField(
-                    hint: 'Entre com seu Usuário',
-                    icon: Icons.person,
-                    labelHint: 'Usuário',
-                    obscure: false,
-                    stream: _loginBloc.outEmail,
-                    onChange: _loginBloc.changeEmail,
-                  ),
-                  InputField(
-                    hint: 'Entre com sua senha',
-                    labelHint: 'Senha',
-                    obscure: true,
-                    icon: Icons.lock,
-                    stream: _loginBloc.outPassword,
-                    onChange: _loginBloc.changePassword,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  StreamBuilder<bool>(
-                      stream: _loginBloc.outSubmitValid,
-                      builder: (context, snapshot) {
-                        return ElevatedButton(
-                          onPressed:
-                              snapshot.hasData ? _loginBloc.subimit : null,
-                          child: Text('Entrar'),
-                          style: ElevatedButton.styleFrom(
-                            onPrimary: Colors.white,
-                            primary: Colors.pinkAccent,
-                            fixedSize: Size(
-                              MediaQuery.of(context).size.width,
-                              50,
+                );
+              case LoginState.FAIL:
+              case LoginState.SUCESS:
+              case LoginState.IDLE:
+                return Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(),
+                    SingleChildScrollView(
+                      child: Container(
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.all(16),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              Icons.store_mall_directory,
+                              color: Colors.pinkAccent,
+                              size: 160,
                             ),
-                            onSurface: Colors.pinkAccent,
-                          ),
-                        );
-                      }),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+                            InputField(
+                              hint: 'Entre com seu Usuário',
+                              icon: Icons.person,
+                              labelHint: 'Usuário',
+                              obscure: false,
+                              stream: _loginBloc.outEmail,
+                              onChange: _loginBloc.changeEmail,
+                            ),
+                            InputField(
+                              hint: 'Entre com sua senha',
+                              labelHint: 'Senha',
+                              obscure: true,
+                              icon: Icons.lock,
+                              stream: _loginBloc.outPassword,
+                              onChange: _loginBloc.changePassword,
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            StreamBuilder<bool>(
+                                stream: _loginBloc.outSubmitValid,
+                                builder: (context, snapshot) {
+                                  return ElevatedButton(
+                                    onPressed: snapshot.hasData
+                                        ? _loginBloc.subimit
+                                        : null,
+                                    child: Text('Entrar'),
+                                    style: ElevatedButton.styleFrom(
+                                      onPrimary: Colors.white,
+                                      primary: Colors.pinkAccent,
+                                      fixedSize: Size(
+                                        MediaQuery.of(context).size.width,
+                                        50,
+                                      ),
+                                      onSurface: Colors.pinkAccent,
+                                    ),
+                                  );
+                                }),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+            }
+          }),
     );
   }
 }
