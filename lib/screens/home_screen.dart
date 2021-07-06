2070cbfcdca0b682e2983aec7dce91b055238b9c
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-07-06 14:26:48 -0300

    pageController

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,8 +8,68 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  PageController? _pageController;
+  int _page = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _page,
+        backgroundColor: Colors.pinkAccent,
+        selectedItemColor: Colors.white70,
+        unselectedItemColor: Colors.white38,
+        onTap: (p) {
+          _pageController!.animateToPage(p,
+              duration: Duration(milliseconds: 500), curve: Curves.ease);
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Clientes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Pedidos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Produtos',
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (p) {
+          setState(() {
+            _page = p;
+          });
+        },
+        children: <Widget>[
+          Container(
+            color: Colors.green,
+          ),
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.red,
+          ),
+        ],
+      ),
+    );
   }
 }
