2a15060ab020e5238f5bf7cc2daead10d36c19b3
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-07-08 10:06:38 -0300

    UserBloc complemento

--- lib/blocs/user_bloc.dart
+++ lib/blocs/user_bloc.dart
@@ -7,6 +7,30 @@ class UserBloc extends BlocBase {
   FirebaseFirestore _firestore = FirebaseFirestore.instance;
   Map<String, Map<String, dynamic>> _users = {};
 
+  UserBloc() {
+    _addUserListener();
+  }
+
+  void _addUserListener() {
+    _firestore.collection('users').snapshots().listen((snapshot) {
+      snapshot.docChanges.forEach((change) {
+        String uid = change.doc.id;
+
+        switch (change.type) {
+          case DocumentChangeType.added:
+            _users[uid] = change.doc.data()!;
+            break;
+          case DocumentChangeType.modified:
+            _users[uid]!.addAll(change.doc.data()!);
+            break;
+          case DocumentChangeType.removed:
+            _users.remove(uid);
+            break;
+        }
+      });
+    });
+  }
+
   @override
   void dispose() {
     super.dispose();
