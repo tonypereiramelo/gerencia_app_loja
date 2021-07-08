19086e61209f315b2b389104984fc8fdd087a9c9
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-07-08 10:43:28 -0300

    UserBloc completo

--- lib/blocs/user_bloc.dart
+++ lib/blocs/user_bloc.dart
@@ -19,16 +19,49 @@ class UserBloc extends BlocBase {
         switch (change.type) {
           case DocumentChangeType.added:
             _users[uid] = change.doc.data()!;
+            _subscribeToOrders(uid);
             break;
           case DocumentChangeType.modified:
             _users[uid]!.addAll(change.doc.data()!);
+            _userController.add(_users.values.toList());
             break;
           case DocumentChangeType.removed:
             _users.remove(uid);
+            _unsubscribeToOrders(uid);
+            _userController.add(_users.values.toList());
             break;
         }
       });
     });
+  }
+
+  void _subscribeToOrders(String uid) {
+    _users[uid]!['subscription'] = _firestore
+        .collection('users')
+        .doc(uid)
+        .collection("orders")
+        .snapshots()
+        .listen((orders) async {
+      int numOrders = orders.docs.length;
+
+      double money = 0.0;
+
+      for (DocumentSnapshot d in orders.docs) {
+        DocumentSnapshot<Map<String, dynamic>> order =
+            await _firestore.collection('orders').doc(d.id).get();
+        if (order.data() == null) continue;
+
+        money += order.data()!['totalPrice'];
+      }
+
+      _users[uid]!.addAll({'money': money, 'orders': numOrders});
+
+      _userController.add(_users.values.toList());
+    });
+  }
+
+  void _unsubscribeToOrders(String uid) {
+    _users[uid]!['subscription'].cancel();
   }
 
   @override
