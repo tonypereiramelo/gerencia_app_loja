ae8f241afccc6db979aff92dceb8d20f6fa475d2
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-07-12 07:27:00 -0300

    OrderTab

---
+++ lib/tabs/order_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OrderTile extends StatelessWidget {
+  const OrderTile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
