ad046506551f5ac535679feee7f66a78fcd4e47f
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-07-14 13:39:14 -0300

    orders_tab com erro no length

--- lib/tabs/orders_tab.dart
+++ lib/tabs/orders_tab.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:gerencia_app_loja/blocs/orders_bloc.dart';
 import 'package:gerencia_app_loja/tabs/order_tile.dart';
 
 class OrdersTab extends StatelessWidget {
@@ -6,14 +8,33 @@ class OrdersTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _ordersBloc = BlocProvider.getBloc<OrdersBloc>();
     return Padding(
       padding: EdgeInsets.symmetric(
         vertical: 16,
       ),
-      child: ListView.builder(
-          itemCount: 6,
-          itemBuilder: (context, index) {
-            return OrderTile();
+      child: StreamBuilder<Object>(
+          stream: _ordersBloc.outOrders,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData)
+              return Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(Colors.pinkAccent),
+                ),
+              );
+            else if (snapshot.data!.length == 0)
+              return Center(
+                child: Text(
+                  'Nenhum pedido encontrado!',
+                  style: TextStyle(color: Colors.pinkAccent),
+                ),
+              );
+            else
+              return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return OrderTile();
+                  });
           }),
     );
   }
