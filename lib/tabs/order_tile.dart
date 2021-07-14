c820caeae7f58b2c502382c668fb67540692b10c
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-07-14 11:47:20 -0300

    tab criada

--- lib/tabs/order_tile.dart
+++ lib/tabs/order_tile.dart
@@ -5,6 +5,56 @@ class OrderTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 4,
+      ),
+      child: Card(
+        child: ExpansionTile(
+          title: Text(
+            '#123456 - Entregue',
+            style: TextStyle(
+              color: Colors.green,
+            ),
+          ),
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                left: 16,
+                right: 16,
+                top: 0,
+                bottom: 8,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListTile(
+                        title: Text(
+                          'Camiseta Preta P',
+                        ),
+                        subtitle: Text(
+                          'Camisetas/dfsjkdhfk',
+                        ),
+                        trailing: Text(
+                          '2',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        contentPadding: EdgeInsets.zero,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
