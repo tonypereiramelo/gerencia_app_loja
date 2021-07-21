8153c35dfecab784f7e182f6133b43fcc7875f08
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-07-21 13:12:33 -0300

    floatingActionButton em pedidos.

--- lib/blocs/orders_bloc.dart
+++ lib/blocs/orders_bloc.dart
@@ -2,11 +2,14 @@ import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:rxdart/rxdart.dart';
 
+enum SortCriteria { READY_FIRST, READY_LAST }
+
 class OrdersBloc extends BlocBase {
   final _ordersController = BehaviorSubject<List>();
   Stream<List> get outOrders => _ordersController.stream;
   FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  List<DocumentSnapshot> _orders = [];
+  List<DocumentSnapshot<Map<String, dynamic>>> _orders = [];
+  late SortCriteria _criteria;
 
   OrdersBloc() {
     _addOrdersListener();
@@ -32,6 +35,41 @@ class OrdersBloc extends BlocBase {
       });
       _ordersController.add(_orders);
     });
+  }
+
+  void setOrderCriteria(SortCriteria criteria) {
+    _criteria = criteria;
+    _sort();
+  }
+
+  void _sort() {
+    switch (_criteria) {
+      case SortCriteria.READY_FIRST:
+        _orders.sort((a, b) {
+          int sa = a.data()!['status'];
+          int sb = b.data()!['status'];
+
+          if (sa < sb) return 1;
+          if (sa > sb)
+            return -1;
+          else
+            return 0;
+        });
+        break;
+      case SortCriteria.READY_LAST:
+        _orders.sort((a, b) {
+          int sa = a.data()!['status'];
+          int sb = b.data()!['status'];
+
+          if (sa > sb) return 1;
+          if (sa < sb)
+            return -1;
+          else
+            return 0;
+        });
+        break;
+    }
+    _ordersController.add(_orders);
   }
 
   @override
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:gerencia_app_loja/blocs/orders_bloc.dart';
 import 'package:gerencia_app_loja/blocs/user_bloc.dart';
 import 'package:gerencia_app_loja/tabs/orders_tab.dart';
@@ -87,6 +88,49 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
+      floatingActionButton: _buildFloating(),
     );
+  }
+
+  Widget? _buildFloating() {
+    switch (_page) {
+      case 0:
+        return null;
+      case 1:
+        return SpeedDial(
+          child: Icon(Icons.sort),
+          backgroundColor: Colors.pinkAccent,
+          overlayOpacity: 0.4,
+          overlayColor: Colors.black,
+          children: [
+            SpeedDialChild(
+                child: Icon(
+                  Icons.arrow_downward,
+                  color: Colors.pinkAccent,
+                ),
+                backgroundColor: Colors.white,
+                label: 'Concluidos Abaixo',
+                labelStyle: TextStyle(
+                  fontSize: 14,
+                ),
+                onTap: () {
+                  _ordersBloc.setOrderCriteria(SortCriteria.READY_LAST);
+                }),
+            SpeedDialChild(
+                child: Icon(
+                  Icons.arrow_upward,
+                  color: Colors.pinkAccent,
+                ),
+                backgroundColor: Colors.white,
+                label: 'Concluidos Acima',
+                labelStyle: TextStyle(
+                  fontSize: 14,
+                ),
+                onTap: () {
+                  _ordersBloc.setOrderCriteria(SortCriteria.READY_FIRST);
+                }),
+          ],
+        );
+    }
   }
 }
